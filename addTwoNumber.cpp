191dd37b5772b301aae1f7a23c8f0d1a5fe9e64d
itczl22 <itczliang@gmail.com>
2016-08-26 12:18:39 +0800

    求两个列表的和(即对应项相加)

--- addTwoNumber.cpp
+++ addTwoNumber.cpp
@@ -1,4 +1,10 @@
-/**
+/*
+ * two linked lists representing two non-negative numbers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list.
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ *
  * Definition for singly-linked list.
  * struct ListNode {
  *   int val;
@@ -13,15 +19,21 @@ class Solution {
             ListNode* prev = new ListNode(-1);
             ListNode* back = prev;
             while(l1 || l2) {
+                //求l1和l2的元素
                 const int ai = !l1 ? 0 : l1->val;
                 const int bi = !l2 ? 0 : l2->val;
+                //求和
                 const int value = (ai + bi + carry) % 10;
+                //求进位
                 carry = (ai + bi + carry) / 10;
+
                 prev->next = new ListNode(value);
                 prev = prev->next;
+                
                 l1 = !l1 ? NULL : l1->next;
                 l2 = !l2 ? NULL : l2->next;
             }
+            //如果有最后一个进位
             if (carry > 0) {
                 prev->next = new ListNode(carry);
             }
