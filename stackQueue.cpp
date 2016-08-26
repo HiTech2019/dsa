a7486e5336736c793bf9acbf5cc572ec41847a72
itczl22 <itczliang@gmail.com>
2016-08-26 11:41:26 +0800

    基于stack的queue

--- stackQueue.cpp
+++ stackQueue.cpp
@@ -2,59 +2,60 @@
  * 基于堆栈的队列
  */
 class Queue {
-public:
-	// 压入
-	void push(int data) {
-		m_i.push(data);
-	}
-	// 弹出
-	void pop(void) {
-		if(m_o.empty()) {
-			if(m_i.empty())
-				throw underflow_error("队列下溢！");
-			while(! m_i.empty()) {
-				m_o.push(m_i.top());
-                m_i.pop();
-            }
-		}
-        m_o.pop();
-	}
-    // 获取尾元素
-    int back(void) {
-        if(m_i.empty()) {
+    public:
+        // 压入
+        void push(int data) {
+            m_i.push(data);
+        }
+        // 弹出
+        void pop(void) {
             if(m_o.empty()) {
-                throw underflow_error("队列下溢");
+                if(m_i.empty()) {
+                    throw underflow_error("队列下溢！");
+                }
+                while(! m_i.empty()) {
+                    m_o.push(m_i.top()); 
+                    m_i.pop();
+                }
             }
-            while(!m_o.empty()) {
-                m_i.push(m_o.top());
-                m_o.pop();
-            }
+            m_o.pop();
         }
-        return m_i.top();
-    }
-    // 获取首元素
-    int front(void) {
-        if(m_o.empty()) {
+        // 获取尾元素
+        int back(void) {
             if(m_i.empty()) {
-                throw underflow_error("队列下溢");
+                if(m_o.empty()) {
+                    throw underflow_error("队列下溢");
+                }
+                while(!m_o.empty()) {
+                    m_i.push(m_o.top());
+                    m_o.pop();
+                }
             }
-            while(!m_i.empty()) {
-                m_o.push(m_i.top());
-                m_i.pop();
-            }
+            return m_i.top();
         }
-        return m_o.top();
-    }
-	// 判空
-	bool empty(void) const {
-		return m_i.empty() && m_o.empty();
-	}
-    // 大小
-    int size(void) {
-        return m_i.size() + m_o.size();
-    }
-private:
-	stack<int> m_i; // 输入栈
-	stack<int> m_o; // 输出栈
+        // 获取首元素
+        int front(void) {
+            if(m_o.empty()) {
+                if(m_i.empty()) {
+                    throw underflow_error("队列下溢");
+                }
+                while(!m_i.empty()) {
+                    m_o.push(m_i.top());
+                    m_i.pop();
+                }
+            }
+            return m_o.top();
+        }
+        // 判空
+        bool empty(void) const {
+            return m_i.empty() && m_o.empty();
+        }
+        // 大小
+        int size(void) {
+            return m_i.size() + m_o.size();
+        }
+    private:
+        stack<int> m_i; // 输入栈
+        stack<int> m_o; // 输出栈
 };
 
