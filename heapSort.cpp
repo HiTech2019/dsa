66009e33adf951ff3b9f81e8cf8e1aa8f6a762a8
itczl22 <itczliang@gmail.com>
2016-09-05 21:10:00 +0800

    堆排序

--- heapSort.cpp
+++ heapSort.cpp
@@ -8,19 +8,17 @@ class HeadSort {
             int left = 2 * i + 1;         //i的左孩子节点下标
             int right = 2 * i + 2;        //i的右孩子节点下标 
             int max = i;                  //记录最大值的下标
-            //如果i是叶节点就不用进行调整 
-            if(i <= len / 2 - 1) {
-                if(left < len && arr[left] > arr[max]) {
-                    max = left;
-                }
-                if(right < len && arr[right] > arr[max]) {
-                    max = right;
-                }
-                if(max != i) {
-                    swap(arr[i], arr[max]);
-                    //避免调整之后以max为父节点的子树不是堆 
-                    heapAdjust(arr, max, len);  
-                }
+            //如果i是叶节点就不用进行调整,if(i <= len / 2 - 1) 调用的时候以确保,所以无需判断
+            if(left < len && arr[left] > arr[max]) {
+                max = left;
+            }
+            if(right < len && arr[right] > arr[max]) {
+                max = right;
+            }
+            if(max != i) {
+                swap(arr[i], arr[max]);
+                //避免调整之后以max为父节点的子树不是堆 
+                heapAdjust(arr, max, len);  
             }
         }
 
