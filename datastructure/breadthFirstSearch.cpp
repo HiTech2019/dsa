f79ac78f346abc4b22d51f70e86c0169ac5099fd
itczl22 <itczliang@gmail.com>
2016-09-28 10:36:42 +0800

    二叉树的广度遍历

--- datastructure/breadthFirstSearch.cpp
+++ datastructure/breadthFirstSearch.cpp
@@ -1,17 +1,19 @@
 class BreadthFirstSearch {
     public:
         void breadFirstSearch(BiTree* root) {
-            queue<BiTree*> nodeQueue;
-            nodeQueue.push(root);
-            while(!nodeQueue.empty()) {
-                BiTree* tmp = nodeQueue.front();
-                cout << tmp->data << ' ';
-                nodeQueue.pop();
-                if(tmp->left) {
-                    nodeQueue.push(tmp->left);
-                }
-                if(tmp->right) {
-                    nodeQueue.push(tmp->right);
+            if(root) {
+                queue<BiTree*> nodeQueue;
+                nodeQueue.push(root);
+                while(!nodeQueue.empty()) {
+                    BiTree* tmp = nodeQueue.front();
+                    cout << tmp->data << ' ';
+                    nodeQueue.pop();
+                    if(tmp->left) {
+                        nodeQueue.push(tmp->left);
+                    }
+                    if(tmp->right) {
+                        nodeQueue.push(tmp->right);
+                    }
                 }
             }
         }
